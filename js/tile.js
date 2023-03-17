1889c65f57b33476eebfb953acf30c4e0a481292
Rain Ren <59131595+HongyiRen2009@users.noreply.github.com>
2023-03-16 17:19:48 -0700

    Update tile.js

--- js/tile.js
+++ js/tile.js
@@ -2,32 +2,50 @@ const tiles = [
     {
         type:"grass",
         image:false,
-        color:"rgb(51, 166, 59)"
+        color:"rgb(51, 166, 59)",
+		r:51,
+		g:166,
+		b:59
     },
     {
         type:"hill",
         image:false,
-        color:"rgb(103, 104, 107)"
+        color:"rgb(103, 104, 107)",
+		r:103,
+		g:104,
+		b:107
     },
     {
         type:"sand",
         image:false,
-        color:"#ffe3a0"
+        color:"rgb(255, 227, 160)",
+		r:255,
+		g:227,
+		b:160
     },
     {
         type:"river",
         image:false,
-        color:"rgb(3,172,252)"
+        color:"rgb(3,172,252)",
+		r:3,
+		g:172,
+		b:252
     },
     {
         type:"desert",
         image:false,
-        color:"#FAD5A5",
-    },
+        color:"rgb(250, 213, 165)",
+		r:241,
+		g:190,
+		b:108
+	},
     {
         type:"jungle",
         image:false,
-        color:"	#315527"
+        color:"rgb(49, 85, 39)",
+		r:49,
+		g:85,
+		b:39
     }
 ]
 function addTile(type,x,y,index=undefined){
@@ -54,10 +72,16 @@ function exists(type,x,y){
     if(tilestats[tilecode(x,y)]==undefined){
         return false
     }
-    
+    if(typeof type == "number"){
     if(tiles[tilestats[tilecode(x,y)].index].type==type&&grid[y].includes(x)){
         return true
     }
+	}
+	else{
+		if(type.includes(tiles[tilestats[tilecode(x,y)].index].type)&&grid[y].includes(x)){
+			return true
+		}
+	}
     return false
 }
 function tilecode(x,y){

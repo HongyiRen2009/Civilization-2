7ce928cc79d339fe25dd18853cdeb987dacb0e22
Rain Ren <59131595+HongyiRen2009@users.noreply.github.com>
2023-04-25 14:02:14 -0700

    Update tile.js

--- js/tile.js
+++ js/tile.js
@@ -1,92 +1,165 @@
 const tiles = [
-    {
-        type:"grass",
-        image:false,
-        color:"rgb(51, 166, 59)",
-		r:51,
-		g:166,
-		b:59
-    },
-    {
-        type:"hill",
-        image:false,
-        color:"rgb(103, 104, 107)",
-		r:103,
-		g:104,
-		b:107
-    },
-    {
-        type:"sand",
-        image:false,
-        color:"rgb(255, 227, 160)",
-		r:255,
-		g:227,
-		b:160
-    },
-    {
-        type:"river",
-        image:false,
-        color:"rgb(3,172,252)",
-		r:3,
-		g:172,
-		b:252
-    },
-    {
-        type:"desert",
-        image:false,
-        color:"rgb(250, 213, 165)",
-		r:241,
-		g:190,
-		b:108
-	},
-    {
-        type:"jungle",
-        image:false,
-        color:"rgb(49, 85, 39)",
-		r:49,
-		g:85,
-		b:39
-    }
-]
-function addTile(type,x,y,index=undefined){
-    
-    tilestats[tilecode(x,y)] = {index:tileindex[type]}
-    if(grid[y].includes(x)){
-        grid[y].splice(grid[y].indexOf(x),1)
-    }
-    if(index==undefined){
-    grid[y].push(x)
-    
-    }
-    else{
-        
-        grid[y].splice(index,0,x)
-    }
+  {
+    type: "grass",
+    image: false,
+    color: "rgb(51, 166, 59)",
+    r: 51,
+    g: 166,
+    b: 59,
+  },
+  {
+    type: "hill",
+    image: false,
+    color: "rgb(103, 104, 107)",
+    r: 103,
+    g: 104,
+    b: 107,
+  },
+  {
+    type: "sand",
+    image: false,
+    color: "rgb(255, 227, 160)",
+    r: 255,
+    g: 227,
+    b: 160,
+  },
+  {
+    type: "river",
+    image: false,
+    color: "rgb(3,172,252)",
+    r: 3,
+    g: 172,
+    b: 252,
+  },
+  {
+    type: "desert",
+    image: false,
+    color: "rgb(241, 190, 108)",
+    r: 241,
+    g: 190,
+    b: 108,
+  },
+  {
+    type: "jungle",
+    image: false,
+    color: "rgb(49, 85, 39)",
+    r: 49,
+    g: 85,
+    b: 39,
+  },
+  {
+    type: "snow",
+    image: false,
+    color: "rgb(255,255,255)",
+    r: 255,
+    g: 255,
+    b: 255,
+  },
+  {
+    type: "ice",
+    image: false,
+    color: "rgb(63,208,212)",
+    r: 63,
+    g: 208,
+    b: 212,
+  },
+  {
+    type: "savanna",
+    image: false,
+    color: "rgb(209,189,146)",
+    r: 209,
+    g: 189,
+    b: 146,
+  },
+];
+function addTile(type, x, y, index = undefined) {
+  tilestats[tilecode(x, y)] = { index: tileindex[type] };
+  if (grid[y].includes(x)) {
+    grid[y].splice(grid[y].indexOf(x), 1);
+  }
+  if (index == undefined) {
+    grid[y].push(x);
+  } else {
+    grid[y].splice(index, 0, x);
+  }
 }
-function removetile(x,y){
-    delete tilestats[tilecode(x,y)]
-    
-    grid[y].splice(grid[y].indexOf(x),1)
+function removetile(x, y) {
+  delete tilestats[tilecode(x, y)];
+
+  grid[y].splice(grid[y].indexOf(x), 1);
 }
-function exists(type,x,y){
-    if(tilestats[tilecode(x,y)]==undefined){
-        return false
+function tiledecode(code) {
+  return {
+    x: parseInt(code.slice(0, code.indexOf("/"))),
+    y: parseInt(code.substr(code.indexOf("/") + 1, code.length)),
+  };
+}
+function updatebuilding(building) {
+  for (const pos of building.positions) {
+    ctx5.clearRect(pos.x * 20, pos.y * 20, 20, 20);
+    if (building.disabled) {
+      ctx5.drawImage(
+        buildimg2,
+        pos.img.dx,
+        pos.img.dy,
+        20,
+        20,
+        pos.x * 20,
+        pos.y * 20,
+        20,
+        20
+      );
+    } else if (!building.inrange) {
+      ctx5.drawImage(
+        buildimg3,
+        pos.img.dx,
+        pos.img.dy,
+        20,
+        20,
+        pos.x * 20,
+        pos.y * 20,
+        20,
+        20
+      );
+    } else {
+      ctx5.drawImage(
+        buildimg,
+        pos.img.dx,
+        pos.img.dy,
+        20,
+        20,
+        pos.x * 20,
+        pos.y * 20,
+        20,
+        20
+      );
     }
-    if(typeof type == "number"){
-    if(tiles[tilestats[tilecode(x,y)].index].type==type&&grid[y].includes(x)){
-        return true
+  }
+}
+function exists(type, x, y) {
+  if (tilestats[tilecode(x, y)] == undefined) {
+    return false;
+  }
+  if (typeof type == "string") {
+    if (
+      tiles[tilestats[tilecode(x, y)].index].type == type &&
+      grid[y].includes(x)
+    ) {
+      return true;
     }
-	}
-	else{
-		if(type.includes(tiles[tilestats[tilecode(x,y)].index].type)&&grid[y].includes(x)){
-			return true
-		}
-	}
-    return false
+  } else {
+    if (
+      type.includes(tiles[tilestats[tilecode(x, y)].index].type) &&
+      grid[y].includes(x)
+    ) {
+      return true;
+    }
+  }
+  return false;
 }
-function tilecode(x,y){
-    return x+"/"+y
+function tilecode(x, y) {
+  return x + "/" + y;
 }
-for (let i=0,len=tiles.length;i<len;i++){
-    tileindex[tiles[i].type]=i
+for (let i = 0, len = tiles.length; i < len; i++) {
+  tileindex[tiles[i].type] = i;
 }
